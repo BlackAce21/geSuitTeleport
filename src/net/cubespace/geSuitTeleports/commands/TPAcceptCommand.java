8bd6e8fd9f317c24597149dc418298268345547b
BlackAce21 <the1blackace21@gmail.com>
2014-05-01 19:07:58 -0400

    Added Data Save methods

--- src/net/cubespace/geSuitTeleports/commands/TPAcceptCommand.java
+++ src/net/cubespace/geSuitTeleports/commands/TPAcceptCommand.java
@@ -1,5 +1,6 @@
 package net.cubespace.geSuitTeleports.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,7 +13,7 @@ public class TPAcceptCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
-
+			Bukkit.getPlayer(sender.getName()).saveData();
 			TeleportsManager.tpAccept(sender);
 			return true;
 	}
